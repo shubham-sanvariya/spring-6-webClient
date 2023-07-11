c1afa89493d4e3361a9e7cb01593918dc0a66383
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-11 14:47:25 +0530

    adding BeerDto model

---
+++ src/main/java/shubh/springframework/spring6webClient/model/BeerDto.java
@@ -0,0 +1,25 @@
+package shubh.springframework.spring6webClient.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BeerDto {
+
+    private String  id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+}
