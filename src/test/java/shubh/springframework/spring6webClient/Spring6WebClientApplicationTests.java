2192c943a4c9bdc55c294e030106cc560f6d133b
shubham-sanvariya <shubhamsanvariyait.com>
2023-07-11 11:43:33 +0530

    Initial commit

---
+++ src/test/java/shubh/springframework/spring6webClient/Spring6WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package shubh.springframework.spring6webClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
